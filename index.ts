29090a5cbe90794f9aec7a44f67746ed7d76484d
Muniox <muniox20@gmail.com>
2022-10-13 14:36:19 +0200

    Add query for test database
    - database test pass

--- index.ts
+++ index.ts
@@ -3,7 +3,18 @@ dotenv.config();
 import express, {Express} from "express";
 const app: Express = express();
 
+//zapytanie test
+import pool from "./utility/db";
+(async () => {
+    try {
+        const [data] = await pool.execute('SELECT * FROM `users`');
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+})();
 
-app.listen(process.env.PORT, () => {
+
+app.listen(Number(process.env.PORT), () => {
     console.log(`server listening on http://localhost:${process.env.PORT}`);
 });
